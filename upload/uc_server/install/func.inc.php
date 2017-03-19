7cb2e3fa9d4156148cdffcd74e9b418aada2428f
Brownsugar <brownsugar@vip.qq.com>
2017-03-20 02:14:09 +0800

    更新 func.inc.php

--- upload/uc_server/install/func.inc.php
+++ upload/uc_server/install/func.inc.php
@@ -747,7 +747,8 @@ function dfopen($url, $limit = 0, $post = '', $cookie = '', $bysocket = FALSE, $
 	$scheme = $matches['scheme'];
 	$host = $matches['host'];
 	$path = $matches['path'] ? $matches['path'].(isset($matches['query']) && $matches['query'] ? '?'.$matches['query'] : '') : '/';
-	$port = !empty($matches['port']) ? $matches['port'] : ($matches['scheme'] == 'https' ? 443 : 80);
+	$port = !empty($matches['port']) ? $matches['port'] : $scheme == 'https' ? 443 : 80;
+	$ishttps = $scheme == 'https';
 
 	if($post) {
 		$out = "POST $path HTTP/1.0\r\n";
@@ -773,7 +774,7 @@ function dfopen($url, $limit = 0, $post = '', $cookie = '', $bysocket = FALSE, $
 	}
 
 	$fpflag = 0;
-	if(!$fp = @fsocketopen(($scheme == 'https' ? 'ssl' : $scheme).'://'.($scheme == 'https' ? $host : ($ip ? $ip : $host)), $port, $errno, $errstr, $timeout)) {
+	if(!$fp = @fsocketopen(($ishttps ? 'ssl://'.$host : ($ip ? $ip : $host)), $port, $errno, $errstr, $timeout)) {
 		$context = array(
 			'http' => array(
 				'method' => $post ? 'POST' : 'GET',
@@ -783,7 +784,7 @@ function dfopen($url, $limit = 0, $post = '', $cookie = '', $bysocket = FALSE, $
 			),
 		);
 		$context = stream_context_create($context);
-		$fp = @fopen($scheme.'://'.($scheme == 'https' ? $host : ($ip ? $ip : $host)).':'.$port.$path, 'b', false, $context);
+		$fp = @fopen($scheme.'://'.($ishttps ? $host : ($ip ? $ip : $host)).':'.$port.$path, 'b', false, $context);
 		$fpflag = 1;
 	}
 
