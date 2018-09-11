821bcf87c53a381d035f8061ed98ad0a5bc68076
Passer <passer.gan@dacsee.cn>
2018-09-11 15:57:58 +0800

    捐助

--- upload/source/plugin/zeroze007_auto/index.inc.php
+++ upload/source/plugin/zeroze007_auto/index.inc.php
@@ -6,6 +6,7 @@ if (!defined('IN_DISCUZ')) {
 //include template('common/header');
 $op = $_G['gp_op'];
 $subop = $_G['gp_subop'];
+$paymoney = $_G['gp_paymoney'];
 if ($op == "account") {
     if (in_array($subop, array('unlock', 'check')) && $_SERVER['REQUEST_METHOD'] == 'POST') {
         $email = trim($_G['gp_email']);
@@ -35,9 +36,21 @@ if ($op == "account") {
             $monthSum = (date("Y", TIMESTAMP) - date("Y", $user['freezetime'])) * 12 + (date("m", TIMESTAMP) - date("m", $user['freezetime']));
             //解封需要悦币
             $needMoney = max(abs($monthSum * 10 + 10), 10);
-            if ($subop == 'unlock' && $needMoney <= $user['extcredits2']) {
-                updatecreditbyaction('', $user['uid'], array('extcredits2' => "-$needMoney"));
-                C::t('common_member')->update($user['uid'], array('status' => 0, 'freezetime' => 0));
+
+            if ($subop == 'unlock' && ($needMoney <= $user['extcredits2'] || $paymoney)) {
+                if ($needMoney <= $user['extcredits2']) {
+                    updatecreditbyaction('', $user['uid'], array('extcredits2' => "-$needMoney"));
+                    C::t('common_member')->update($user['uid'], array('status' => 0, 'freezetime' => 0));
+                } elseif ($paymoney) {
+                    updatecreditbyaction('', $user['uid'], array('extcredits2' => "-" . abs($user['extcredits2'])));
+                    C::t('common_member')->update($user['uid'], array('status' => 0, 'freezetime' => 0));
+                    C::t('common_member_status')->update($user['uid'], array('lastpost' => time()));
+                }
+                notification_add(
+                    $user['uid'], 'system', '温馨提示：为避免再次被系统锁定，请24小时内回复任意主题，以解除违规状态！<br/>总版规：<a href="forum.php?mod=viewthread&tid=2">前往查看</a><br/>如有疑问请至接待中心反馈!',
+                    array(),
+                    1
+                );
                 showmessage('您的账号已解除锁定，请前往登录！', 'member.php?mod=logging&action=login', array(), array('showmsg' => true, 'login' => 1));
             } else {
                 include template('zeroze007_auto:auto_unlock_confirm');
