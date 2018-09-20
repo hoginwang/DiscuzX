f1d168cd580189a241fa8b6e68a24076fcedfd2f
Passer <passer.gan@dacsee.cn>
2018-09-20 16:31:37 +0800

    fix

--- upload/static/js/webupload.js
+++ upload/static/js/webupload.js
@@ -2,9 +2,6 @@ var WEBUpload;
 
 
 if (WEBUpload == undefined) {
-    if(!window.jQuery) {
-        appendscript(STATICURL + 'js/jquery.min.js?' + VERHASH);
-    }
     if (window.jQuery) {
         jQuery.noConflict();
     }
