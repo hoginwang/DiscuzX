287f93b7b568fb2af62f67d4be842be150c935fd
老周部落 <laozhoubuluo@126.com>
2018-12-30 09:00:20 +0800

    修复install.php中uchidden参数的XSS漏洞

--- upload/install/include/install_function.php
+++ upload/install/include/install_function.php
@@ -348,6 +348,14 @@ function show_env_result(&$env_items, &$dirfile_items, &$func_items, &$filesock_
 
 function show_next_step($step, $error_code) {
 	global $uchidden;
+
+	if(!empty($uchidden)) {//Check $uchidden from user, fix xss.
+		$uc_info_transfer = unserialize(urldecode($uchidden));
+		if(!isset($uc_info_transfer['ucapi']) && !isset($uc_info_transfer['ucfounderpw'])){
+			$uchidden = '';
+		}
+	}
+
 	echo "<form action=\"index.php\" method=\"post\">\n";
 	echo "<input type=\"hidden\" name=\"step\" value=\"$step\" />";
 	if(isset($GLOBALS['hidden'])) {
