95a37c6b0d5cd46593138f36f38dfa762c3a1430
老周部落 <laozhoubuluo@126.com>
2019-08-17 22:44:08 +0800

    修复 后台UCenter设置中MySQL数据库服务器允许在线编辑引发任意文件读取漏洞

--- upload/source/admincp/admincp_setting.php
+++ upload/source/admincp/admincp_setting.php
@@ -2632,7 +2632,7 @@ EOT;
 		$settingnew['uc']['key'] = addslashes($settingnew['uc']['key'] == '********' ? addslashes(UC_KEY) : $settingnew['uc']['key']);
 
 		if(function_exists("mysql_connect") && ini_get("mysql.allow_local_infile")=="1" && constant("UC_DBHOST") != $settingnew['uc']['dbhost']){
-			cpmsg('uc_config_load_data_infile_error', '', 'error');
+			cpmsg('uc_config_load_data_local_infile_error', '', 'error');
 		}
 
 		if($settingnew['uc']['connect']) {
