4c8f6ec38e70b721e4d50ecf531f4456849d03fc
ytjxzxy <527340870@qq.com>
2019-11-09 09:59:33 +0800

    修改论坛远程图片本地化，修复其不能本地化带参数图片的BUG,使其可以本地化从任何网站复制到的内容。

--- upload/source/module/forum/forum_ajax.php
+++ upload/source/module/forum/forum_ajax.php
@@ -360,127 +360,129 @@ if($_GET['action'] == 'checkusername') {
 		dexit();
 	}
 	$_GET['message'] = str_replace(array("\r", "\n"), array($_GET['wysiwyg'] ? '<br />' : '', "\\n"), $_GET['message']);
-	preg_match_all("/\[img\]\s*([^\[\<\r\n]+?)\s*\[\/img\]|\[img=\d{1,4}[x|\,]\d{1,4}\]\s*([^\[\<\r\n]+?)\s*\[\/img\]/is", $_GET['message'], $image1, PREG_SET_ORDER);
-	preg_match_all("/\<img.+src=('|\"|)?(.*)(\\1)([\s].*)?\>/ismUe", $_GET['message'], $image2, PREG_SET_ORDER);
-	$temp = $aids = $existentimg = array();
-	if(is_array($image1) && !empty($image1)) {
-		foreach($image1 as $value) {
-			$temp[] = array(
-				'0' => $value[0],
-				'1' => trim(!empty($value[1]) ? $value[1] : $value[2])
-			);
-		}
-	}
-	if(is_array($image2) && !empty($image2)) {
-		foreach($image2 as $value) {
-			$temp[] = array(
-				'0' => $value[0],
-				'1' => trim($value[2])
-			);
-		}
-	}
-	require_once libfile('class/image');
-	if(is_array($temp) && !empty($temp)) {
-		$upload = new discuz_upload();
-		$attachaids = array();
-
-		foreach($temp as $value) {
-			$imageurl = $value[1];
-			$hash = md5($imageurl);
-			if(strlen($imageurl)) {
-				$imagereplace['oldimageurl'][] = $value[0];
-				if(!isset($existentimg[$hash])) {
-					$existentimg[$hash] = $imageurl;
-					$attach['ext'] = $upload->fileext($imageurl);
-					if(!$upload->is_image_ext($attach['ext'])) {
-						continue;
-					}
-					$content = '';
-					if(preg_match('/^(http:\/\/|\.)/i', $imageurl)) {
-						$content = dfsockopen($imageurl);
-					} elseif(preg_match('/^('.preg_quote(getglobal('setting/attachurl'), '/').')/i', $imageurl)) {
-						$imagereplace['newimageurl'][] = $value[0];
-					}
-					if(empty($content)) continue;
-					$patharr = explode('/', $imageurl);
-					$attach['name'] =  trim($patharr[count($patharr)-1]);
-					$attach['thumb'] = '';
-
-					$attach['isimage'] = $upload -> is_image_ext($attach['ext']);
-					$attach['extension'] = $upload -> get_target_extension($attach['ext']);
-					$attach['attachdir'] = $upload -> get_target_dir('forum');
-					$attach['attachment'] = $attach['attachdir'] . $upload->get_target_filename('forum').'.'.$attach['extension'];
-					$attach['target'] = getglobal('setting/attachdir').'./forum/'.$attach['attachment'];
-
-					if(!@$fp = fopen($attach['target'], 'wb')) {
-						continue;
-					} else {
-						flock($fp, 2);
-						fwrite($fp, $content);
-						fclose($fp);
-					}
-					if(!$upload->get_image_info($attach['target'])) {
-						@unlink($attach['target']);
-						continue;
-					}
-					$attach['size'] = filesize($attach['target']);
-					$upload->attach = $attach;
-					$thumb = $width = 0;
-					if($upload->attach['isimage']) {
-						if($_G['setting']['thumbsource'] && $_G['setting']['sourcewidth'] && $_G['setting']['sourceheight']) {
-							$image = new image();
-							$thumb = $image->Thumb($upload->attach['target'], '', $_G['setting']['sourcewidth'], $_G['setting']['sourceheight'], 1, 1) ? 1 : 0;
-							$width = $image->imginfo['width'];
-							$upload->attach['size'] = $image->imginfo['size'];
-						}
-						if($_G['setting']['thumbstatus']) {
-							$image = new image();
-							$thumb = $image->Thumb($upload->attach['target'], '', $_G['setting']['thumbwidth'], $_G['setting']['thumbheight'], $_G['setting']['thumbstatus'], 0) ? 1 : 0;
-							$width = $image->imginfo['width'];
-						}
-						if($_G['setting']['thumbsource'] || !$_G['setting']['thumbstatus']) {
-							list($width) = @getimagesize($upload->attach['target']);
-						}
-						if($_G['setting']['watermarkstatus'] && empty($_G['forum']['disablewatermark'])) {
-							$image = new image();
-							$image->Watermark($attach['target'], '', 'forum');
-							$upload->attach['size'] = $image->imginfo['size'];
-						}
-					}
-					$aids[] = $aid = getattachnewaid();
-					$setarr = array(
-						'aid' => $aid,
-						'dateline' => $_G['timestamp'],
-						'filename' => $upload->attach['name'],
-						'filesize' => $upload->attach['size'],
-						'attachment' => $upload->attach['attachment'],
-						'isimage' => $upload->attach['isimage'],
-						'uid' => $_G['uid'],
-						'thumb' => $thumb,
-						'remote' => '0',
-						'width' => $width
-					);
-					C::t("forum_attachment_unused")->insert($setarr);
-					$attachaids[$hash] = $imagereplace['newimageurl'][] = '[attachimg]'.$aid.'[/attachimg]';
-
-				} else {
-					$imagereplace['newimageurl'][] = $attachaids[$hash];
-				}
-			}
-		}
-		if(!empty($aids)) {
-			require_once libfile('function/post');
-		}
-		$_GET['message'] = str_replace($imagereplace['oldimageurl'], $imagereplace['newimageurl'], $_GET['message']);
-	}
-	$_GET['message'] = addcslashes($_GET['message'], '/"\'');
-	print <<<EOF
+    preg_match_all("/\[img\]\s*([^\[\<\r\n]+?)\s*\[\/img\]|\[img=\d{1,4}[x|\,]\d{1,4}\]\s*([^\[\<\r\n]+?)\s*\[\/img\]/is", $_GET['message'], $image1, PREG_SET_ORDER);
+    preg_match_all("/\<img.+src=('|\"|)?(.*)(\\1)([\s].*)?\>/ismUe", $_GET['message'], $image2, PREG_SET_ORDER);
+    $temp = $aids = $existentimg = array();
+    if(is_array($image1) && !empty($image1)) {
+        foreach($image1 as $value) {
+            $tempstr = trim(!empty($value[1]) ? $value[1] : $value[2]);
+            $temp[] = array(
+                '0' => $value[0],
+                '1' => $tempstr
+            );
+        }
+    }
+    if(is_array($image2) && !empty($image2)) {
+        foreach($image2 as $value) {
+            $tempstr = trim($value[2]);
+            $temp[] = array(
+                '0' => $value[0],
+                '1' => $tempstr
+            );
+        }
+    }
+    require_once libfile('class/image');
+    if(is_array($temp) && !empty($temp)) {
+        $upload = new discuz_upload();
+        $attachaids = array();
+    
+        foreach($temp as $value) {
+            $imageurl = $value[1];
+            $hash = md5($imageurl);
+            if(strlen($imageurl)) {
+                $imagereplace['oldimageurl'][] = $value[0];
+                if(!isset($existentimg[$hash])) {
+                    $existentimg[$hash] = $imageurl;
+                    //$attach['ext'] = $upload->fileext($imageurl);
+                    $attach['ext'] = 'jpg';
+                    if(!$upload->is_image_ext($attach['ext'])) {
+                        continue;
+                    }
+                    $content = '';
+                    if(preg_match('/^(https?:\/\/|\.)/i', $imageurl)) {
+                        $content = dfsockopen($imageurl);
+                    } elseif(preg_match('/^('.preg_quote(getglobal('setting/attachurl'), '/').')/i', $imageurl)) {
+                        $imagereplace['newimageurl'][] = $value[0];
+                    }
+                    if(empty($content)) continue;
+                    $patharr = explode('/', $imageurl);
+                    $attach['name'] =  trim($patharr[count($patharr)-1]);
+                    $attach['thumb'] = '';
+                    $attach['isimage'] = $upload -> is_image_ext($attach['ext']);
+                    $attach['extension'] = $upload -> get_target_extension($attach['ext']);
+                    $attach['attachdir'] = $upload -> get_target_dir('forum');
+                    $attach['attachment'] = $attach['attachdir'] . $upload->get_target_filename('forum').'.'.$attach['extension'];
+                    $attach['target'] = getglobal('setting/attachdir').'./forum/'.$attach['attachment'];
+    
+                    if(!@$fp = fopen($attach['target'], 'wb')) {
+                        continue;
+                    } else {
+                        flock($fp, 2);
+                        fwrite($fp, $content);
+                        fclose($fp);
+                    }
+                    if(!$upload->get_image_info($attach['target'])) {
+                        @unlink($attach['target']);
+                        continue;
+                    }
+                    $attach['size'] = filesize($attach['target']);
+                    $upload->attach = $attach;
+                    $thumb = $width = 0;
+                    if($upload->attach['isimage']) {
+                        if($_G['setting']['thumbsource'] && $_G['setting']['sourcewidth'] && $_G['setting']['sourceheight']) {
+                            $image = new image();
+                            $thumb = $image->Thumb($upload->attach['target'], '', $_G['setting']['sourcewidth'], $_G['setting']['sourceheight'], 1, 1) ? 1 : 0;
+                            $width = $image->imginfo['width'];
+                            $upload->attach['size'] = $image->imginfo['size'];
+                        }
+                        if($_G['setting']['thumbstatus']) {
+                            $image = new image();
+                            $thumb = $image->Thumb($upload->attach['target'], '', $_G['setting']['thumbwidth'], $_G['setting']['thumbheight'], $_G['setting']['thumbstatus'], 0) ? 1 : 0;
+                            $width = $image->imginfo['width'];
+                        }
+                        if($_G['setting']['thumbsource'] || !$_G['setting']['thumbstatus']) {
+                            list($width) = @getimagesize($upload->attach['target']);
+                        }
+                        if($_G['setting']['watermarkstatus'] && empty($_G['forum']['disablewatermark'])) {
+                            $image = new image();
+                            $image->Watermark($attach['target'], '', 'forum');
+                            $upload->attach['size'] = $image->imginfo['size'];
+                        }
+                    }
+                    $aids[] = $aid = getattachnewaid();
+                    $setarr = array(
+                        'aid' => $aid,
+                        'dateline' => $_G['timestamp'],
+                        'filename' => $upload->attach['name'],
+                        'filesize' => $upload->attach['size'],
+                        'attachment' => $upload->attach['attachment'],
+                        'isimage' => $upload->attach['isimage'],
+                        'uid' => $_G['uid'],
+                        'thumb' => $thumb,
+                        'remote' => '0',
+                        'width' => $width
+                    );
+                    C::t("forum_attachment_unused")->insert($setarr);
+                    $attachaids[$hash] = $imagereplace['newimageurl'][] = '[attachimg]'.$aid.'[/attachimg]';
+    
+                } else {
+                    $imagereplace['newimageurl'][] = $attachaids[$hash];
+                }
+            }
+        }
+        if(!empty($aids)) {
+            require_once libfile('function/post');
+        }
+        $_GET['message'] = str_replace($imagereplace['oldimageurl'], $imagereplace['newimageurl'], $_GET['message']);
+    }
+    $_GET['message'] = addcslashes($_GET['message'], '/"\'');
+print <<<EOF
 		<script type="text/javascript">
 			parent.ATTACHORIMAGE = 1;
 			parent.updateDownImageList('$_GET[message]');
 		</script>
 EOF;
-	dexit();
+dexit();
 } elseif($_GET['action'] == 'exif') {
 	$exif = C::t('forum_attachment_exif')->fetch($_GET['aid']);
 	$s = $exif['exif'];
