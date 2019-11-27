cd0b3189617a351a77d346058eddacb1d6617f66
oldhu <me@oldhu.com>
2019-11-27 23:13:42 +0800

    加了一点注释

--- upload/source/module/forum/forum_viewthread.php
+++ upload/source/module/forum/forum_viewthread.php
@@ -1277,14 +1277,14 @@ function replace_formhash($input) {
 
 function viewthread_loadcache() {
 	global $_G;
-	$_G['thread']['livedays'] = ceil((TIMESTAMP - $_G['thread']['dateline']) / 86400);
-	$_G['thread']['lastpostdays'] = ceil((TIMESTAMP - $_G['thread']['lastpost']) / 86400);
+	$_G['thread']['livedays'] = ceil((TIMESTAMP - $_G['thread']['dateline']) / 86400);	// 本贴子存在了多少天，最少是1天
+	$_G['thread']['lastpostdays'] = ceil((TIMESTAMP - $_G['thread']['lastpost']) / 86400);	// 最后发帖天数，最少1天
 
 	$threadcachemark = 100 - (
-		$_G['thread']['digest'] * 20 +
-		min($_G['thread']['views'] / max($_G['thread']['livedays'], 10) * 2, 50) +
-		max(-10, (15 - $_G['thread']['lastpostdays'])) +
-		min($_G['thread']['replies'] / $_G['setting']['postperpage'] * 1.5, 15));
+		$_G['thread']['digest'] * 20 +							// 精华，占20分
+		min($_G['thread']['views'] / max($_G['thread']['livedays'], 10) * 2, 50) +	// 阅读数与天数关系，占50分。阅读越多分越高，天数越久分越低
+		max(-10, (15 - $_G['thread']['lastpostdays'])) +				// 最后回复时间，占15分，超过15天开始倒扣分，最多扣10分
+		min($_G['thread']['replies'] / $_G['setting']['postperpage'] * 1.5, 15));	// 帖子页数，占15分，10页以上就是满分
 	if($threadcachemark < $_G['forum']['threadcaches']) {
 
 		$threadcache = getcacheinfo($_G['tid']);
