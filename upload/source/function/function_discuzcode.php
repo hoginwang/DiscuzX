23d3f9083b03ba29832a3d40692e4db3cb105949
孤独的味道ﺭ <1182630+gududeweidao@user.noreply.gitee.com>
2020-01-06 09:55:22 +0800

    媒体附件显示直链 mp3, flac, ape, mp4, flv, mov, avi 后缀的文件，通过 attach:// 引用的话，显示附件直链
    需后台设置
    
    全局-上传设置-论坛附件-  附件 URL 地址、媒体附件播放: 是

--- upload/source/function/function_discuzcode.php
+++ upload/source/function/function_discuzcode.php
@@ -376,6 +376,7 @@ function parseattachurl($aid, $ext, $ignoretid = 0) {
 	$_G['forum_skipaidlist'][] = $aid;
 	if($ext === 'mp3' || $ext === 'flac' || $ext === 'ape' || $ext === 'mp4' || $ext === 'flv' || $ext === 'mov' || $ext === 'avi'){
 	$attach = C::t('forum_attachment_n')->fetch('aid:'.$aid, $aid);
+	return $_G['setting']['attachurl'].'forum/'.$attach['attachment'];
 	}
 	else{
 	return $_G['siteurl'].'forum.php?mod=attachment&aid='.aidencode($aid, $ext, $ignoretid ? '' : $_G['tid']).($ext ? '&request=yes&_f=.'.$ext : '');
