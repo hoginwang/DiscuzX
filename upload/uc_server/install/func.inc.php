1303d31de0c7f1d4f6e79b9f1a03fa94e50e4c57
老周部落 <laozhoubuluo@126.com>
2020-01-09 01:19:50 +0800

    新增 UCenter安装文件(MySQLi Only)

--- upload/uc_server/install/func.inc.php
+++ upload/uc_server/install/func.inc.php
@@ -638,12 +638,12 @@ function authcode($string, $operation = 'DECODE', $key = '', $expiry = 0) {
 function generate_key() {
 	$random = random(32);
 	$info = md5($_SERVER['SERVER_SOFTWARE'].$_SERVER['SERVER_NAME'].$_SERVER['SERVER_ADDR'].$_SERVER['SERVER_PORT'].$_SERVER['HTTP_USER_AGENT'].time());
-	$return = '';
+	$return = array();// 按Discuz!安装文件进行修改, Git新增
 	for($i=0; $i<64; $i++) {
 		$p = intval($i/2);
 		$return[$i] = $i % 2 ? $random[$p] : $info[$p];
 	}
-	return $return;// 原有意义不明的逻辑为 return implode('', $return);
+	return implode('', $return);
 }
 
 function show_install() {
