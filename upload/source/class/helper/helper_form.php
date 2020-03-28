c23ceb557787fdda4eebaac856d303415f844e74
ZhaiSoul <842607283@qq.com>
2020-03-28 21:07:11 +0800

    修改 为并发锁增加设置选项

--- upload/source/class/helper/helper_form.php
+++ upload/source/class/helper/helper_form.php
@@ -20,7 +20,9 @@ class helper_form {
 		} else {
 			global $_G;
 			register_shutdown_function('helper_form::submitcheck_unlock', $var);
-			if(!discuz_process::islocked($var.'_'.$_G['uid'].'_locked', 60)) {
+			if($_G['setting']['submitlock'] && discuz_process::islocked($var.'_'.$_G['uid'].'_locked', 60)) {
+				showmessage('submit_islocked');
+			} else {
 				if($allowget || ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_GET['formhash']) && $_GET['formhash'] == formhash() && empty($_SERVER['HTTP_X_FLASH_VERSION']) && (empty($_SERVER['HTTP_REFERER']) ||
 					strncmp($_SERVER['HTTP_REFERER'], 'http://wsq.discuz.com/', 22) === 0 || preg_replace("/https?:\/\/([^\:\/]+).*/i", "\\1", $_SERVER['HTTP_REFERER']) == preg_replace("/([^\:]+).*/", "\\1", $_SERVER['HTTP_HOST'])))) {
 					if(checkperm('seccode')) {
@@ -35,8 +37,6 @@ class helper_form {
 				} else {
 					showmessage('submit_invalid');
 				}
-			} else {
-				showmessage('submit_islocked');
 			}
 		}
 	}
