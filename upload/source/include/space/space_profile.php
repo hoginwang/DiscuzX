fd664b3cec9aa23b1a320c201871e9f3d1064431
xslidian <xslidian@gmail.com>
2020-04-03 12:52:18 +0800

    修复用户分表后用户资料页缺字段的问题 /home.php?mod=space&uid=*&do=profile

--- upload/source/include/space/space_profile.php
+++ upload/source/include/space/space_profile.php
@@ -13,11 +13,12 @@ if(!defined('IN_DISCUZ')) {
 
 require_once libfile('function/spacecp');
 
-space_merge($space, 'count');
-space_merge($space, 'field_home');
-space_merge($space, 'field_forum');
-space_merge($space, 'profile');
-space_merge($space, 'status');
+$inarchive = isset($space['_inarchive']) && $space['_inarchive'];
+space_merge($space, 'count', $inarchive);
+space_merge($space, 'field_home', $inarchive);
+space_merge($space, 'field_forum', $inarchive);
+space_merge($space, 'profile', $inarchive);
+space_merge($space, 'status', $inarchive);
 getonlinemember(array($space['uid']));
 
 if($space['videophoto'] && ckvideophoto($space, 1)) {
