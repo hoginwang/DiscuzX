0772fbb677fe926c9fb71a05e50616c8cb57201c
木子三有 <9710@163.com>
2020-05-05 16:04:52 +0800

    兼容更新高分率图片上传（8K） 程序当前的设置，最大只能上传4096*4096的图片，可能在未来发展上有些不足，改成8192*8192 。

--- upload/source/class/discuz/discuz_upload.php
+++ upload/source/class/discuz/discuz_upload.php
@@ -114,7 +114,7 @@ Class discuz_upload{
 		} elseif($imageinfo = @getimagesize($target)) {
 			list($width, $height, $type) = !empty($imageinfo) ? $imageinfo : array('', '', '');
 			$size = $width * $height;
-			if($size > 16777216 || $size < 16 ) {
+			if($size > 67108864 || $size < 16 ) {
 				return false;
 			} elseif($ext == 'swf' && $type != 4 && $type != 13) {
 				return false;
