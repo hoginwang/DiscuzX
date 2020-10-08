2a1b50c0591a102dcfd57bee644fa2fcd5c5b2a4
老周部落 <laozhoubuluo@126.com>
2020-10-08 23:35:23 +0800

    修复 除零错误

--- upload/source/module/forum/forum_misc.php
+++ upload/source/module/forum/forum_misc.php
@@ -726,7 +726,7 @@ if($_GET['action'] == 'votepoll' && submitcheck('pollsubmit', 1)) {
 		}
 		C::t('forum_post')->increase_rate_by_pid('tid:'.$_G['tid'], $_GET['pid'], $rate, $ratetimes);
 		if($post['first']) {
-			$threadrate = intval(@($post['rate'] + $rate) / abs($post['rate'] + $rate));
+			$threadrate = intval((abs($post['rate'] + $rate) ? (($post['rate'] + $rate) / abs($post['rate'] + $rate)) : 0));
 			C::t('forum_thread')->update($_G['tid'], array('rate'=>$threadrate));
 
 		}
@@ -862,7 +862,7 @@ if($_GET['action'] == 'votepoll' && submitcheck('pollsubmit', 1)) {
 			}
 			C::t('forum_post')->increase_rate_by_pid('tid:'.$_G['tid'], $_GET['pid'], $rate, $ratetimes);
 			if($post['first']) {
-				$threadrate = @intval(@($post['rate'] + $rate) / abs($post['rate'] + $rate));
+				$threadrate = intval((abs($post['rate'] + $rate) ? (($post['rate'] + $rate) / abs($post['rate'] + $rate)) : 0));
 				C::t('forum_thread')->update($_G['tid'], array('rate'=>$threadrate));
 			}
 
