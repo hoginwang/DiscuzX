a643f4e2c9c616fb0418499677a6fd8526c75bb8
老周部落 <laozhoubuluo@126.com>
2020-10-09 23:25:32 +0800

    修复 QQ互联插件调用

--- upload/source/plugin/qqconnect/lib/ConnectOAuth.php
+++ upload/source/plugin/qqconnect/lib/ConnectOAuth.php
@@ -211,7 +211,7 @@ class Cloud_Service_Client_ConnectOAuth extends Cloud_Service_Client_OAuth {
 	private function _iconv($data, $inputCharset, $outputCharset) {
 		if (is_array($data)) {
 			foreach($data as $key => $val) {
-				$value = array_map(array(__CLASS__, '_iconv'), array($val), array($inputCharset), array($outputCharset));
+				$value = array_map(array($this, '_iconv'), array($val), array($inputCharset), array($outputCharset));
 				$result[$key] = $value[0];
 			}
 		} else {
