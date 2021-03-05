39a42ba23ac2498124d451d6ff107fa8e560a407
DiscuzX <153619335@qq.com>
2021-03-05 20:35:51 +0800

    数据库备份恢复功能小调整及补充语言包

--- utility/restore.php
+++ utility/restore.php
@@ -80,13 +80,13 @@ if($operation == 'import') {
 			$confirm = $confirm ? 1 : 0;
 			$sqldump = fgets($fp, 256);
 			$identify = explode(',', base64_decode(preg_replace("/^# Identify:\s*(\w+).*/s", "\\1", $sqldump)));
-			$dumpinfo = array('method' => $identify[3], 'volume' => intval($identify[4]), 'tablepre' => $identify[5], 'dbcharset' => $identify[6]);
+			$dumpinfo = array('method' => $identify[3], 'volume' => intval($identify[4]), 'tablepre' => $identify[5], 'dbcharset' => strtolower($identify[6]));
 			if(!$confirm) {
 				$showmsg = '';
 				if($dumpinfo['tablepre'] != $_config['db']['1']['tablepre'] && !getgpc('ignore_tablepre', 'G')) {
 					$showmsg .= lang('tableprediff');
 				}
-				if($dumpinfo['dbcharset'] != $_config['db']['1']['dbcharset']) {
+				if($dumpinfo['dbcharset'] != strtolower($_config['db']['1']['dbcharset'])) {
 					$showmsg .= lang('dbcharsetdiff');
 				}
 				if($showmsg) {
@@ -123,7 +123,6 @@ if($operation == 'import') {
 				}
 			}
 
-			$delunzip = getgpc('delunzip', 'G');
 			if($delunzip) {
 				@unlink($datafile);
 			}
@@ -348,7 +347,7 @@ function show_importfile_list($exportlog = array(), $exportziplog = array(), $ex
 				'<td></td>',
 				"<td>".$info['size']."</td>",
 				'<td></td>',
-				"<td>".$info['volume']."</td>",
+				"<td></td>",
 				'<td></td>'
 			;
 			echo "</tr>\n";
@@ -361,13 +360,14 @@ function show_importfile_list($exportlog = array(), $exportziplog = array(), $ex
 		$info = $val[0];
 		$info['dateline'] = is_int($info['dateline']) ? gmdate('Y-m-d H:i:s', $info['dateline'] + 3600 * 8) : lang('unknown');
 		$info['size'] = sizecount($exportzipsize[$key]);
+		$info['volume'] = count($val);
 		$info['method'] = $info['method'] == 'multivol' ? lang('db_multivol') : lang('db_zip');
 		echo "<tr>";
 		echo
 			"<td><a href=\"javascript:;\" onclick=\"display('exportlog_zip_$key')\">".basename($info['filename'])."</a></td>",
 			"<td colspan='2'>".dirname($info['filename'])."</td>",
 			"<td width='140'>".$info['dateline']."</td>",
-			"<td width='170'>".lang('db_export_'.$info['type'])."</td>",
+			"<td width='170'>".($info['volume'] > 1 ? lang('db_multivol') : '').lang('db_export_'.$info['type'])."</td>",
 			"<td width='80'>".$info['size']."</td>",
 			"<td colspan='2'>".$info['method']."</td>",
 			"<td width='40'><a href=\"{$siteurl}restore.php?operation=importzip&datafile_server=$info[filename]&importsubmit=yes\" onclick=\"return confirm('".lang('database_import_confirm_zip')."');\">".lang('db_import_unzip')."</a></td>"
@@ -384,7 +384,7 @@ function show_importfile_list($exportlog = array(), $exportziplog = array(), $ex
 				"<td>".$info['dateline']."</td>",
 				"<td>".lang('db_export_'.$info['type'])."</td>",
 				"<td>".$info['size']."</td>",
-				"<td colspan='3'>".$info['method']."</td>"
+				"<td colspan='3'></td>"
 			;
 			echo "</tr>\n";
 		}
@@ -533,7 +533,7 @@ function show_msg($message, $url_forward = '', $type = 'message', $success = 0) 
 		echo "<script>setTimeout(\"redirect('$url_forward');\", 1250);</script>";
 	} elseif($type == 'confirm') {
 		echo "$message";
-		echo "<br /><br /><br /><button id=\"confirmbtn\" onclick=\"redirect('$url_forward')\">确定</button><button id=\"cancelbtn\" onclick=\"redirect('{$siteurl}restore.php')\">取消</button>";
+		echo "<br /><br /><br /><button id=\"confirmbtn\" onclick=\"redirect('$url_forward')\">确定</button>&nbsp;<button id=\"cancelbtn\" onclick=\"redirect('{$siteurl}restore.php')\">取消</button>";
 	}
 
 	show_footer();
@@ -583,46 +583,50 @@ function show_tips($tip, $title = '', $comment = '', $style = 1) {
 }
 
 function lang($lang_key, $force = true, $replace = array()) {
-	$lang = array('filename' => '文件名称',
-				'director' => '所在目录',
-				'version' => '版本',
-				'time' => '备份时间',
-				'type' => '类型',
-				'size' => '尺寸',
-				'db_method' => '方式',
-				'db_volume' => '卷数',
-				'import' => '导入',
-				'different_dbcharset_tablepre' => '检测到导入的备份数据与配置文件的{diff} 不同，您还要继续运行此程序吗？',
-				'db_import_tips' => '本功能在恢复备份数据的同时，将全部覆盖原有数据，请确定恢复前已将论坛关闭，恢复全部完成后可以将论坛重新开放。<br />您可以通过数据备份管理功能查看站点的备份文件的详细信息，删除过期的备份,并导入需要的备份。<br /><span class="red">恢复数据的整个过程会在一个新页面完成，您成功恢复数据后请务必及时删除restore.php文件。</span><br />',
-				'db_export_discuz' => 'Discuz! 数据(不含UCenter)',
-				'db_export_discuz_uc' => 'Discuz! 和 UCenter 数据',
-				'db_multivol' => '多卷',
-				'db_import_unzip' => '解压缩',
-				'db_export_custom' => '自定义备份',                
-				'db_export_zip' => '压缩备份',
-				'db_zip' => 'ZIP',
-				'db_shell' => 'Shell',
-				'unknown' => '未知',
-				'backup_file_unexist' => '备份文件不存在',
-				'connect_error' => '连接数据库失败，请您查看数据库配置文件config/config_global.php和config/config_ucenter.php是否存在以及配置是否正确',
-				'dbcharsetdiff' => ' 数据库字符集($_config[\'db\'][\'1\'][\'dbcharset\'])',
-				'tableprediff' => ' 表前缀($_config[\'db\'][\'1\'][\'tablepre\'])',
-				'database_import_multivol_succeed' => '分卷数据成功导入站点数据库<br />请在后台更新缓存<br /><span class="red">请尽快删除restore.php文件，以免对数据造成影响</span>',
-				'database_import_file_illegal' => '数据文件不存在：可能服务器不允许上传文件或文件大小超过限制',
-				'database_import_multivol_prompt' => '分卷数据第一卷成功导入数据库，您需要自动导入本次备份的其他分卷吗？',
-				'database_import_succeed' => '数据已成功导入站点数据库<br />请在后台更新缓存<br /><span class="red">请尽快删除restore.php文件，以免对数据造成影响</span>',
-				'database_import_format_illegal' => '数据文件非 Discuz! 格式，无法导入',
-				'database_import_unzip' => '{info}<br />备份文件解压缩完毕，您需要自动导入备份吗？导入后解压缩的文件将会被删除',
-				'database_import_multivol_unzip' => '{info}<br />备份文件解压缩完毕，您需要自动解压缩其他的分卷文件吗？',
-				'database_import_multivol_unzip_redirect' => '数据文件 #{multivol} 解压缩成功，程序将自动继续',
-				'database_import_confirm' => '导入和当前 Discuz! 版本不一致的数据极有可能产生无法解决的故障，您确定继续吗？',
-				'database_import_confirm_sql' => '您确定导入该备份吗？',
-				'database_import_confirm_zip' => '您确定解压该备份吗？',
-				'database_import_multivol_confirm' => '所有分卷文件解压缩完毕，您需要自动导入备份吗？导入后解压缩的文件将会被删除',
-				'database_import_multivol_redirect' => '数据文件 #{volume} 成功导入，程序将自动继续',
-				'error_quit_msg' => '必须解决以上问题，才能继续恢复数据',
-				'restored_error' => '恢复数据功能锁定，已经恢复过了，如果您确定要恢复数据，请到服务器上删除./data/restore.lock',
-			);
+	global $_config;
+	$lang = array(
+		'filename' => '文件名称',
+		'director' => '所在目录',
+		'version' => '版本',
+		'time' => '备份时间',
+		'type' => '类型',
+		'size' => '尺寸',
+		'db_method' => '方式',
+		'db_volume' => '卷数',
+		'import' => '导入',
+		'different_dbcharset_tablepre' => '检测到导入的备份数据与配置文件的{diff} 不同，您还要继续运行此程序吗？',
+		'db_import_tips' => '本功能在恢复备份数据的同时，将全部覆盖原有数据，请确定恢复前已将论坛关闭，恢复全部完成后可以将论坛重新开放。<br />您可以通过数据备份管理功能查看站点的备份文件的详细信息，删除过期的备份,并导入需要的备份。<br /><span class="red">恢复数据的整个过程会在一个新页面完成，您成功恢复数据后请务必及时删除restore.php文件。</span><br />',
+		'db_export_discuz' => 'Discuz! 数据(不含UCenter)',
+		'db_export_discuz_uc' => 'Discuz! 和 UCenter 数据',
+		'db_multivol' => '多卷',
+		'db_import_unzip' => '解压缩',
+		'db_export_custom' => '自定义备份',                
+		'db_export_zip' => '压缩备份',
+		'db_zip' => 'ZIP',
+		'db_shell' => 'Shell',
+		'unknown' => '未知',
+		'backup_file_unexist' => '备份文件不存在',
+		'connect_error' => '连接数据库失败，请您查看数据库配置文件config/config_global.php和config/config_ucenter.php是否存在以及配置是否正确',
+		'dbcharsetdiff' => ' 数据库字符集('.$_config['db']['1']['dbcharset'].')',
+		'tableprediff' => ' 表前缀('.$_config['db']['1']['tablepre'].')',
+		'database_import_multivol_succeed' => '分卷数据成功导入站点数据库<br />请在后台更新缓存<br /><span class="red">请尽快删除restore.php文件，以免对数据造成影响</span>',
+		'database_import_file_illegal' => '数据文件不存在：可能服务器不允许上传文件或文件大小超过限制',
+		'database_import_multivol_prompt' => '分卷数据第一卷成功导入数据库，您需要自动导入本次备份的其他分卷吗？',
+		'database_import_succeed' => '数据已成功导入站点数据库<br />请在后台更新缓存<br /><span class="red">请尽快删除restore.php文件，以免对数据造成影响</span>',
+		'database_import_format_illegal' => '数据文件非 Discuz! 格式，无法导入',
+		'database_import_unzip' => '{info}<br />备份文件解压缩完毕，您需要自动导入备份吗？导入后解压缩的文件将会被删除',
+		'database_import_multivol_unzip' => '{info}<br />备份文件解压缩完毕，您需要自动解压缩其他的分卷文件吗？',
+		'database_import_multivol_unzip_start' => '正在解压备份文件，程序将自动继续',
+		'database_import_multivol_unzip_redirect' => '数据文件 #{multivol} 解压缩成功，程序将自动继续',
+		'database_import_confirm' => '导入和当前 Discuz! 版本不一致的数据极有可能产生无法解决的故障，您确定继续吗？',
+		'database_import_confirm_sql' => '您确定导入该备份吗？',
+		'database_import_confirm_zip' => '您确定解压该备份吗？',
+		'database_import_multivol_confirm' => '所有分卷文件解压缩完毕，您需要自动导入备份吗？导入后解压缩的文件将会被删除',
+		'database_import_multivol_start' => '正在导入备份文件，程序将自动继续',
+		'database_import_multivol_redirect' => '数据文件 #{volume} 成功导入，程序将自动继续',
+		'error_quit_msg' => '必须解决以上问题，才能继续恢复数据',
+		'restored_error' => '恢复数据功能锁定，已经恢复过了，如果您确定要恢复数据，请到服务器上删除./data/restore.lock',
+	);
 	$return = isset($lang[$lang_key]) ? $lang[$lang_key] : ($force ? $lang_key : '');
 	if($replace && is_array($replace)) {
 		$searchs = $replaces = array();
