85b305b53e6037036987900c0c618a3527688b07
湖中沉 <341936031@qq.com>
2021-04-20 11:38:45 +0800

    修复重置密码邮件链接被邮箱转义导致的参数无效问题

--- upload/source/module/member/member_getpasswd.php
+++ upload/source/module/member/member_getpasswd.php
@@ -13,23 +13,32 @@ if(!defined('IN_DISCUZ')) {
 
 define('NOROBOT', TRUE);
 
-if($_GET['uid'] && $_GET['id'] && $_GET['sign'] === make_getpws_sign($_GET['uid'], $_GET['id'])) {
+$paramexist = preg_match_all('/(\?|&(amp;)?)(.+?)=([^&?]*)/', $_SERVER['QUERY_STRING'], $parammatchs);
+if($paramexist){
+	foreach($parammatchs[4] as $paramk => $paramv){
+		$param[$parammatchs[3][$paramk]] = $paramv;
+	}
+}
+$uid = isset($_GET['uid']) ? $_GET['uid'] : $param['uid'];
+$id = isset($_GET['id']) ? $_GET['id'] : $param['id'];
+$sign = isset($_GET['sign']) ? $_GET['sign'] : $param['sign'];
+
+if($uid && $id && $sign === make_getpws_sign($uid, $id)) {
 
 	$discuz_action = 141;
 
 
-	$member = getuserbyuid($_GET['uid'], 1);
+	$member = getuserbyuid($uid, 1);
 	$table_ext = isset($member['_inarchive']) ? '_archive' : '';
-	$member = array_merge(C::t('common_member_field_forum'.$table_ext)->fetch($_GET['uid']), $member);
+	$member = array_merge(C::t('common_member_field_forum'.$table_ext)->fetch($uid), $member);
 	list($dateline, $operation, $idstring) = explode("\t", $member['authstr']);
 
-	if($dateline < TIMESTAMP - 86400 * 3 || $operation != 1 || $idstring != $_GET['id']) {
+	if($dateline < TIMESTAMP - 86400 * 3 || $operation != 1 || $idstring != $id) {
 		showmessage('getpasswd_illegal', NULL);
 	}
 
 	if(!submitcheck('getpwsubmit') || $_GET['newpasswd1'] != $_GET['newpasswd2']) {
-		$hashid = $_GET['id'];
-		$uid = $_GET['uid'];
+		$hashid = $id;
 		include template('member/getpasswd');
 	} else {
 		if($_GET['newpasswd1'] != addslashes($_GET['newpasswd1'])) {
@@ -65,8 +74,8 @@ if($_GET['uid'] && $_GET['id'] && $_GET['sign'] === make_getpws_sign($_GET['uid'
 		if(isset($member['_inarchive'])) {
 			C::t('common_member_archive')->move_to_master($member['uid']);
 		}
-		C::t('common_member')->update($_GET['uid'], array('password' => $password));
-		C::t('common_member_field_forum')->update($_GET['uid'], array('authstr' => ''));
+		C::t('common_member')->update($uid, array('password' => $password));
+		C::t('common_member_field_forum')->update($uid, array('authstr' => ''));
 		showmessage('getpasswd_succeed', 'index.php', array(), array('login' => 1));
 	}
 
