98a1efab0e9607b0a4ffb486fac2ca49f1e835a5
brotherand2 <547996854@qq.com>
2021-06-04 18:45:51 +0800

    修复 相册-编辑相册-隐私设置对自己空间相册的评论权限不生效

--- upload/source/function/function_comment.php
+++ upload/source/function/function_comment.php
@@ -70,8 +70,8 @@ function add_comment($message, $id, $idtype, $cid = 0) {
 
 			$album = array();
 			if($pic['albumid']) {
-				$query = C::t('home_album')->fetch($pic['albumid']);
-				if(!$query['albumid']) {
+				$album = C::t('home_album')->fetch($pic['albumid']);
+				if(!$album['albumid']) {
 					C::t('home_pic')->update_for_albumid($albumid, array('albumid' => 0));
 				}
 			}
