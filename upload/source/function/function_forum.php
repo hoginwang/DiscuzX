1c9736f916110cad3ee27cfca9909d0d06d7af36
orzi! <1063614727@qq.com>
2021-06-22 14:36:26 +0800

    修复报错

--- upload/source/function/function_forum.php
+++ upload/source/function/function_forum.php
@@ -128,7 +128,7 @@ function formulaperm($formula) {
 			}
 		}
 	}
-	$formulaperm = $formula ? TRUE : FALSE;
+	$formulaperm = $formula ? true : false;
 	if(!$formulaperm) {
 		if(!$permmessage) {
 			$language = lang('forum/misc');
@@ -235,7 +235,7 @@ function medalformulaperm($formula, $type) {
 			}
 		}
 	}
-	$formulaperm = $formula ? TRUE : FALSE;
+	$formulaperm = $formula ? true : false;
 	if(!$formulaperm || $type == 2) {
 		if(!$permmessage) {
 			$language = lang('forum/misc');
