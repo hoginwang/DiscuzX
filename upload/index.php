a342f10106179379d242053af9425f79db1a0a31
rentianji <119721671@qq.com>
2021-07-10 23:22:37 +0800

    测试

--- upload/index.php
+++ upload/index.php
@@ -13,6 +13,7 @@ if(!empty($_SERVER['QUERY_STRING']) && is_numeric($_SERVER['QUERY_STRING'])) {
 } else {
 
 	$url = '';
+	$ren = '';
 	$domain = $_ENV = array();
 	$jump = false;
 	@include_once './data/sysdata/cache_domain.php';
