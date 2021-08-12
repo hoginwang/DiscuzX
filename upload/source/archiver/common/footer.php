72dd80548fb26953ec003a632e5297ba4caeb407
CN-traveler <cn-traveler@qq.com>
2021-08-12 19:53:47 +0800

    使用样式替换center已过时的标签

--- upload/source/archiver/common/footer.php
+++ upload/source/archiver/common/footer.php
@@ -4,14 +4,14 @@ if(!defined('IN_DISCUZ')) {
 }
 ?>
 <br />
-<center>
+<div class="center">
 	<?php echo adshow('footerbanner//1').adshow('footerbanner//2').adshow('footerbanner//3'); ?>
 	<div id="footer">
 		Powered by <strong><a target="_blank" href="http://www.discuz.net">Discuz! <?php echo $_G['setting']['version']; ?> Archiver</a></strong> &nbsp; Copyright &copy 2001-2021, Tencent Cloud.
 		<br />
 		<br />
 	</div>
-</center>
+</div>
 </body>
 </html>
 <?php output(); ?>
--- upload/source/archiver/common/header.php
+++ upload/source/archiver/common/header.php
@@ -22,15 +22,16 @@ ob_start();
 	body {font-family: Verdana;FONT-SIZE: 12px;MARGIN: 0;color: #000000;background: #ffffff;}
 	img {border:0;}
 	li {margin-top: 8px;}
+	.center {text-align: center;}
 	.page {padding: 4px; border-top: 1px #EEEEEE solid}
 	.author {background-color:#EEEEFF; padding: 6px; border-top: 1px #ddddee solid}
 	#nav, #content, #end {padding: 8px; border: 1px solid #EEEEEE; clear: both; width: 95%; margin: auto; margin-top: 10px;}
-	#header, #footer { margin-top: 20px; }
+	#header, #footer { margin-top: 20px; text-align: center;}
 	#loginform {text-align: center;}
 </style>
 </head>
 <body vlink="#333333" link="#333333">
-<center id="header">
+<div id="header" class="center">
 <?php echo adshow('headerbanner'); ?>
 <h2><?php echo $_G['setting']['bbname']; ?>'s Archiver </h2>
-</center>
+</div>
