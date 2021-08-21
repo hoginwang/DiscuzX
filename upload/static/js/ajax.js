c6d81b83d5eac5d3033946f68ac46de9bb5277ee
ACGI <317753971@qq.com>
2021-08-21 02:50:06 +0000

    update upload/static/js/ajax.js. 原_ajaxpost里的waitid参数只传入但并未做任何使用，导致该参数无实际意义，现已修复。

--- upload/static/js/ajax.js
+++ upload/static/js/ajax.js
@@ -52,6 +52,10 @@ function _ajaxpost(formid, showid, waitid, showidclass, submitbtn, recall) {
 	var curform = $(formid);
 	var formtarget = curform.target;
 
+	var showloading = function(display) {
+		$(waitid).style.display = display || 'block';
+		$(waitid).innerHTML = '<span><div class="loadicon vm"></div>请稍候...</span>';
+	};
 	var handleResult = function() {
 		var s = '';
 		var evaled = false;
