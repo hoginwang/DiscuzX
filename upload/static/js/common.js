936421376b9400ac4f5ba8098b5758a98e71ac45
ACGI <317753971@qq.com>
2021-09-11 07:05:06 +0000

    update upload/static/js/common.js.

--- upload/static/js/common.js
+++ upload/static/js/common.js
@@ -1559,7 +1559,7 @@ function addClass(elem, className) {
 }
 
 function removeClass(elem, className) {
-	elem.className = trim(elem.className.replace(new RegExp("(^|\\s)" + className+"(\\s|$)"), " "));
+	elem.className = trim(elem.className.replace(new RegExp("(^|\\s)" + className + "(\\s|$)"), " "));
 }
 
 function toggleClass(elem, className) {
