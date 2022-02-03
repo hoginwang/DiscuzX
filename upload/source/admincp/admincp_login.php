db977d8b5ab5b57303a701b57d4bca9712e365de
老周部落 <laozhoubuluo@126.com>
2022-02-03 14:37:26 +0000

    修复 前台登录才允许登录管理中心对既有站生效的 Bug

--- upload/source/admincp/admincp_login.php
+++ upload/source/admincp/admincp_login.php
@@ -26,7 +26,7 @@ if($this->cpaccess == -3) {
 if(!getglobal('uid') && getglobal('config/admincp/mustlogin')) {
 	echo  '<p class="logintips">'.lang('admincp_login', 'login_cp_guest').'</p>';
 
-} elseif(!getstatus(getglobal('member/allowadmincp'), 1) || $this->cpaccess == -3) {
+} elseif((getglobal('uid') && !getstatus(getglobal('member/allowadmincp'), 1)) || $this->cpaccess == -3) {
 	echo  '<p class="logintips">'.lang('admincp_login', 'login_cp_noaccess').'</p>';
 
 
