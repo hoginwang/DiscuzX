3bf33889050b27d585104d05e4eeef2835deaf4b
Discuz开发者杰瑞科技 <695832763@qq.com>
2022-02-13 07:01:01 +0000

    update upload/source/function/function_core.php. 适配Chrome 80后版本要求第三方地址Set-Cookie必须携带SameSite=None;参数情况下的UCenter同步登录

--- upload/source/function/function_core.php
+++ upload/source/function/function_core.php
@@ -280,11 +280,15 @@ function dsetcookie($var, $value = '', $life = 0, $prefix = 1, $httponly = false
 	$path = $httponly && PHP_VERSION < '5.2.0' ? $config['cookiepath'].'; HttpOnly' : $config['cookiepath'];
 
 	$secure = $_G['isHTTPS'];
-	if(PHP_VERSION < '5.2.0') {
-		setcookie($var, $value, $life, $path, $config['cookiedomain'], $secure);
-	} else {
-		setcookie($var, $value, $life, $path, $config['cookiedomain'], $secure, $httponly);
-	}
+    if(defined('IN_API')){
+        header("Set-Cookie: {$var}=".urlencode($value)."; expires=".date("D, d M Y H:i:s",$life)." GMT; domain={$config['cookiedomain']}; path={$config['cookiepath']}; HTTPOnly; Secure; SameSite=None;",false);
+    }else{
+	    if(PHP_VERSION < '5.2.0') {
+		    setcookie($var, $value, $life, $path, $config['cookiedomain'], $secure);
+	    } else {
+		    setcookie($var, $value, $life, $path, $config['cookiedomain'], $secure, $httponly);
+	    }
+    }
 }
 
 function getcookie($key) {
