718e0ee24627b1a6f27b7e0a1a7b428efc72870d
老周部落 <laozhoubuluo@126.com>
2022-02-15 22:45:48 +0800

    新增 登录、卡密充值功能支持验证问答、短信功能同时支持验证码和验证问答

--- upload/source/admincp/admincp_setting.php
+++ upload/source/admincp/admincp_setting.php
@@ -1859,7 +1859,9 @@ EOT;
 		showsetting('setting_sec_secqaa_status', array('settingnew[secqaa][status]', array(
 			cplang('setting_sec_seccode_status_register'),
 			cplang('setting_sec_seccode_status_post'),
-			cplang('setting_sec_seccode_status_password')
+			cplang('setting_sec_seccode_status_password'),
+			cplang('setting_sec_seccode_status_login'),
+			cplang('setting_sec_seccode_status_card')
 		)), $setting['secqaa']['status'], 'binmcheckbox');
 		showsetting('setting_sec_secqaa_minposts', 'settingnew[secqaa][minposts]', $setting['secqaa']['minposts'], 'text');
 		showtablefooter();
@@ -3043,7 +3045,7 @@ EOT;
 
 		updatecache('secqaa');
 
-		$settingnew['secqaa']['status'] = bindec(intval($settingnew['secqaa']['status'][3]).intval($settingnew['secqaa']['status'][2]).intval($settingnew['secqaa']['status'][1]));
+		$settingnew['secqaa']['status'] = bindec(intval($settingnew['secqaa']['status'][5]).intval($settingnew['secqaa']['status'][4]).intval($settingnew['secqaa']['status'][3]).intval($settingnew['secqaa']['status'][2]).intval($settingnew['secqaa']['status'][1]));
 		$settingnew['secqaa'] = serialize($settingnew['secqaa']);
 
 	} elseif($operation == 'sec') {
