dc48ad85c06e0fb2ae9285d03dae865c52141444
POPBOY <116544+xiangfz@user.noreply.gitee.com>
2022-02-16 16:43:26 +0800

    no commit message

--- upload/source/class/db/db_driver_mysqli.php
+++ upload/source/class/db/db_driver_mysqli.php
@@ -98,7 +98,7 @@ class db_driver_mysqli
 	function table_name($tablename) {
 		if(!empty($this->map) && !empty($this->map[$tablename])) {
 			$id = $this->map[$tablename];
-			if(!empty($this->config[$id]['tablepre'])){
+			if(isset($this->config[$id]['tablepre'])){
 				$this->tablepre = $this->config[$id]['tablepre'];
 			}
 			if(!$this->link[$id]) {
