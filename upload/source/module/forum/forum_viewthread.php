3cccabed0d6863d6e7d3d4b2ada212f7cb5318f5
brotherand2 <547996854@qq.com>
2022-04-27 06:35:59 +0000

    修复  帖内主题帖子在删除或审核状态时倒序浏览楼层显示异常 帖子异常,如删除在回收站,审核状态时,会在forum_threaddisablepos表记录,position字段就失效了,要重新排楼层,倒序时会发生楼层异常显示 修改如下: 1 当帖子有推荐,置顶时,推荐置顶的帖子算了楼层编号,第一页本来是减到10,变成多减了几次,第一页末尾楼层编号会偏小,导致第二页第1个编号比第一页最后一个编号大很多,编号不连续,所以加上curpagepids记录当前页面编号,推荐置顶的是其它页面的不减编号 2 最后一页楼层显示异常,如https://www.dismall.com/forum.php?mod=viewthread&tid=73&extra=page%3D1&ordertype=0&page=51 最后一页有7条,编号应是7-2,显示的是6-1,forum_numpost 修改为($page == $totalpage ? 1 : 0),最后一页减多了,所以要加回去1 3 第二页,显示异常,如https://www.dismall.com/forum.php?mod=viewthread&tid=73&extra=page%3D1&ordertype=1&page=2,第1个帖子显示498,第1页最后一个也显示498 所以
    修改后测试示例:https://zengyonghua.site/discuz35/forum.php?mod=viewthread&tid=3

--- upload/source/module/forum/forum_viewthread.php
+++ upload/source/module/forum/forum_viewthread.php
@@ -489,7 +489,7 @@ if(empty($_GET['viewpid'])) {
 		$_G['forum_numpost'] = ($page - 1) * $_G['ppp'];
 		if($ordertype != 1) {
 		} else {
-			$_G['forum_numpost'] = $_G['forum_thread']['replies'] + 2 - $_G['forum_numpost'] + ($page > 1 ? 1 : 0);
+			$_G['forum_numpost'] = $_G['forum_thread']['replies'] + 2 - $_G['forum_numpost'] + ($page == $totalpage ? 1 : 0);
 		}
 	} else {
 		$start_limit = $_G['forum_numpost'] = max(0, ($page - 1) * $_G['ppp']);
@@ -499,7 +499,7 @@ if(empty($_GET['viewpid'])) {
 		}
 		if($ordertype != 1) {
 		} else {
-			$_G['forum_numpost'] = $_G['forum_thread']['replies'] + 2 - $_G['forum_numpost'] + ($page > 1 ? 1 : 0);
+			$_G['forum_numpost'] = $_G['forum_thread']['replies'] + 2 - $_G['forum_numpost'];
 		}
 	}
 	$multipage = multi($_G['forum_thread']['replies'] + 1, $_G['ppp'], $page, 'forum.php?mod=viewthread&tid='.$_G['tid'].
@@ -623,6 +623,10 @@ if(!empty($isdel_post)) {
 	$ordertype != 1 ? ksort($postarr) : krsort($postarr);
 }
 $summary = '';
+$curpagepids = [];
+foreach($postarr as $post) {
+	$curpagepids[] = $post['pid'];
+}
 if($page == 1 && $ordertype == 1) {
 	$firstpost = C::t('forum_post')->fetch_threadpost_by_tid_invisible($_G['tid']);
 	if($firstpost['invisible'] == 0 || $visibleallflag == 1) {
@@ -667,6 +671,7 @@ foreach($postarr as $post) {
 			$post['existinfirstpage'] = true;
 		}
 
+		$post['incurpage'] = in_array($post['pid'],$curpagepids);
 		$postusers[$post['authorid']] = array();
 		if($post['first']) {
 			if($ordertype == 1 && $page != 1) {
@@ -1075,12 +1080,12 @@ function viewthread_procpost($post, $lastvisit, $ordertype, $maxposition = 0) {
 
 	$post['lastpostanchor'] = ($ordertype != 1 && $_G['forum_numpost'] == $_G['forum_thread']['replies']) || ($ordertype == 1 && $_G['forum_numpost'] == $_G['forum_thread']['replies'] + 2) ? '<a name="lastpost"></a>' : '';
 
-	if(!$post['hotrecommended']) {
+	if(!$post['hotrecommended'] && $post['incurpage']) {
 		if($_G['forum_pagebydesc']) {
 			if($ordertype != 1) {
 				$post['number'] = $_G['forum_numpost'] + $_G['forum_ppp2']--;
 			} else {
-				$post['number'] = $post['first'] == 1 ? 1 : ($_G['forum_numpost'] - 1) - $_G['forum_ppp2']--;
+				$post['number'] = $post['first'] == 1 ? 1 : $_G['forum_numpost']  - $_G['forum_ppp2']--;
 			}
 		} else {
 			if($ordertype != 1) {
@@ -1091,7 +1096,7 @@ function viewthread_procpost($post, $lastvisit, $ordertype, $maxposition = 0) {
 		}
 	}
 
-	if($post['existinfirstpage']) {
+	if($post['existinfirstpage'] && $post['incurpage']) {
 		if($_G['forum_pagebydesc']) {
 			$_G['forum_ppp2']--;
 		} else {
