06b398e52864e7d2e6614a636f5552feb05cd223
brotherand2 <547996854@qq.com>
2022-05-15 07:37:08 +0000

    修复 抢楼帖楼层已中奖数错误 多减了1,导致抢到一个时,显示暂时还没有楼层中奖 示例地址:https://zengyonghua.site/discuz34/forum.php?mod=viewthread&tid=5&checkrush=1

--- upload/source/module/forum/forum_viewthread.php
+++ upload/source/module/forum/forum_viewthread.php
@@ -431,7 +431,7 @@ if(empty($_GET['viewpid'])) {
 				$rushids[$var] = $var;
 			}
 			$temp_reply = $_G['forum_thread']['replies'];
-			$_G['forum_thread']['replies'] = $countrushpost = max(0, count($rushids) - 1);
+			$_G['forum_thread']['replies']  = max(0, count($rushids) - 1);$countrushpost = max(0, count($rushids));
 			$rushids = array_slice($rushids, ($page - 1) * $_G['ppp'], $_G['ppp']);
 			foreach(C::t('forum_post')->fetch_all_by_tid_position($posttableid, $_G['tid'], $rushids) as $post) {
 				$postarr[$post['position']] = $post;
