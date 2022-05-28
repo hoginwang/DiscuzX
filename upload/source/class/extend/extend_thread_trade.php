5097e0af6f7551c354544208f258c3eed386d615
brotherand2 <547996854@qq.com>
2022-05-28 10:31:22 +0000

    修复 商品主题动态链接有误打不开

--- upload/source/class/extend/extend_thread_trade.php
+++ upload/source/class/extend/extend_thread_trade.php
@@ -107,6 +107,7 @@ class extend_thread_trade extends extend_thread_base {
 			'item_credit' => $_GET['item_credit'],
 			'item_costcredit' => $_GET['item_costcredit']
 		));
+		$this->param['tradepid'] = $pid;		
 
 		if(!empty($_GET['tradeaid'])) {
 			convertunusedattach($_GET['tradeaid'], $this->tid, $pid);
@@ -132,6 +133,7 @@ class extend_thread_trade extends extend_thread_base {
 				'itemcredit'=> $_GET['item_credit'],
 				'creditunit'=> $this->setting['extcredits'][$this->setting['creditstransextra'][5]]['unit'].$this->setting['extcredits'][$this->setting['creditstransextra'][5]]['title']
 			);
+			$pid = $this->param['tradepid'];			
 			if($_GET['tradeaid']) {
 				$this->feed['images'] = array(getforumimg($_GET['tradeaid']));
 				$this->feed['image_links'] = array("forum.php?mod=viewthread&do=tradeinfo&tid=".$this->tid."&pid=$pid");
