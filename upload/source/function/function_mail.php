83be075a172ebba486793f0093c622722d2c09d7
“tianshi2” <yangqh1230@gmail.com>
2022-06-11 13:59:50 +0800

    修复smtp的格式

--- upload/source/function/function_mail.php
+++ upload/source/function/function_mail.php
@@ -34,10 +34,10 @@ function sendmail($toemail, $subject, $message = '', $from = '') {
 		$smtpnum = count($_G['setting']['mail']['smtp']);
 		if($smtpnum) {
 			$rid = rand(0, $smtpnum-1);
-			$maildomain = explode('@',$toemail);
-			foreach(array_column($_G['setting']['mail']['smtp'], 'precedence') as $smtpkey => $smtpval){
-				$ismail = in_array($maildomain[1],explode(',',$smtpval));
-				if($ismail !== false){
+			$maildomain = explode('@', $toemail);
+			foreach(array_column($_G['setting']['mail']['smtp'], 'precedence') as $smtpkey => $smtpval) {
+				$ismail = in_array($maildomain[1], explode(',', $smtpval));
+				if($ismail !== false) {
 					$rid = $smtpkey;
 					break;
 				}
