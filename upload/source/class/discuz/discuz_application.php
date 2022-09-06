1a73c547685c3845feb3254640ad1727340e773c
一只玉桂喵 <9486415+MoeCinnamo@user.noreply.gitee.com>
2022-09-06 01:48:08 +0000

    update upload/source/class/discuz/discuz_application.php. 添加手机访问默认域名后自动跳转到手机域名
    Signed-off-by: 一只玉桂喵 <9486415+MoeCinnamo@user.noreply.gitee.com>

--- upload/source/class/discuz/discuz_application.php
+++ upload/source/class/discuz/discuz_application.php
@@ -817,6 +817,11 @@ class discuz_application extends discuz_base{
 			}
 		}
 
+        if($_SERVER['HTTP_HOST'] == $this->var['setting']['domain']['app']['default'] && !$nomobile){
+		    dheader('Location:'.$this->var['scheme'].'://'.$this->var['setting']['domain']['app']['mobile'].$_SERVER['REQUEST_URI']);
+		    return false;
+		}
+
 		if($nomobile || (!$this->var['setting']['mobile']['mobileforward'] && !$mobileflag)) {
 			if($_SERVER['HTTP_HOST'] == $this->var['setting']['domain']['app']['mobile'] && $this->var['setting']['domain']['app']['default']) {
 				dheader('Location:'.$this->var['scheme'].'://'.$this->var['setting']['domain']['app']['default'].$_SERVER['REQUEST_URI']);
