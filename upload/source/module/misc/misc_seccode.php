47983c67323d9168f7a7be204cae734b03bc1ddd
DiscuzX <153619335@qq.com>
2023-02-17 08:32:26 +0000

    验证码输入框的TABINDEX
    Signed-off-by: DiscuzX <153619335@qq.com>

--- upload/source/module/misc/misc_seccode.php
+++ upload/source/module/misc/misc_seccode.php
@@ -66,7 +66,7 @@ if($('$showid')) {
 	if(!$('v$showid')) {
 		var sectpl = seccheck_tpl['$idhash'] != '' ? seccheck_tpl['$idhash'].replace(/<hash>/g, 'code$idhash') : '';
 		var sectplcode = sectpl != '' ? sectpl.split('<sec>') : Array('<br />',': ','<br />','');
-		var string = '<input name="seccodehash" type="hidden" value="$idhash" /><input name="seccodemodid" type="hidden" value="$modid" />' + sectplcode[0] + '{$seclang['seccode']}' + sectplcode[1] + '<input name="seccodeverify" id="seccodeverify_$idhash" type="text" autocomplete="off" style="{$imemode}width:100px" class="txt px vm" onblur="checksec(\'code\', \'$idhash\', 0, null, \'$modid\')" />' +
+		var string = '<input name="seccodehash" type="hidden" value="$idhash" /><input name="seccodemodid" type="hidden" value="$modid" />' + sectplcode[0] + '{$seclang['seccode']}' + sectplcode[1] + '<input name="seccodeverify" id="seccodeverify_$idhash" type="text" autocomplete="off" style="{$imemode}width:100px" class="txt px vm" tabindex="1" onblur="checksec(\'code\', \'$idhash\', 0, null, \'$modid\')" />' +
 			' <a href="javascript:;" onclick="updateseccode(\'$idhash\');doane(event);" class="xi2">{$seclang['seccode_update']}</a>' +
 			'<span id="checkseccodeverify_$idhash"><i class="fico-checkbox fic4 fc-t fnmr vm"></i></span>' +
 			sectplcode[2] + '<span id="v$showid">$message</span>' + sectplcode[3];
