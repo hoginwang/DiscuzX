9582d8d911ebc4e3a66e82dc3bed64e0e473dd20
babyfacer <896667643@qq.com>
2023-06-18 09:57:03 +0000

    部分位置http更新为https
    Signed-off-by: babyfacer <896667643@qq.com>

--- upload/static/js/threadsort.js
+++ upload/static/js/threadsort.js
@@ -225,7 +225,7 @@ function checkoption(identifier, required, checktype, checkmaxnum, checkminnum, 
 				return false;
 			}
 		} else if(checktype == 'url' && !(/(http[s]?|ftp):\/\/[^\/\.]+?\..+\w[\/]?$/i.test(checkvalue))) {
-			warning(ce, '请正确填写以http://开头的URL地址');
+			warning(ce, '请正确填写以http:// 或https:// 开头的URL地址');
 			return false;
 		}
 		ce.innerHTML = '<i class="fico-check_right fic4 fc-v vm"></i>';
