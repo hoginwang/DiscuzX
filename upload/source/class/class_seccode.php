37e0eda1eb63cd1ea52eeb3f300198c7ccc11f4a
Eiffel2028 <h.y.leung@icloud.com>
2024-03-14 09:07:02 +0000

    PHP 小问题 mt_rand(): Argument #2 ($max) must be greater than or equal to argument #1 ($min) PHP:misc.php#require(%s):0096 source/module/misc/misc_seccode.php#seccode->seccode->display():0187 source/class/class_seccode.php#seccode->seccode->image():0042 source/class/class_seccode.php#seccode->seccode->ttffont():0084 source/class/class_seccode.php#mt_rand():0221 source/class/class_seccode.php#break():0221
    Signed-off-by: Eiffel2028 <h.y.leung@icloud.com>

--- upload/source/class/class_seccode.php
+++ upload/source/class/class_seccode.php
@@ -218,7 +218,9 @@ class seccode {
 			$font[$i]['width'] = $font[$i]['width'] > $this->width / $seccodelength ? $this->width / $seccodelength : $font[$i]['width'];
 			$widthtotal += $font[$i]['width'];
 		}
-		$x = mt_rand($font[0]['angle'] > 0 ? cos(deg2rad(90 - $font[0]['angle'])) * $font[0]['zheight'] : 1, $this->width - $widthtotal);
+        $min=$font[0]['angle'] > 0 ? cos(deg2rad(90 - $font[0]['angle'])) * $font[0]['zheight'] : 1;
+        $max=$this->width - $widthtotal;
+        $x = $max < $min ? mt_rand($max,$min) : mt_rand($min, $max);
 		!$this->color && $text_color = imagecolorallocate($this->im, $this->fontcolor[0], $this->fontcolor[1], $this->fontcolor[2]);
 		for($i = 0; $i < $seccodelength; $i++) {
 			if($this->color) {
