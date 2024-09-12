03666e91fd8903cf8473290702a391d7afa93d93
Hmily_LCG <68857640@qq.com>
2024-09-12 08:32:47 +0000

    update upload/source/include/topicadmin/topicadmin_moderate.php. 修复同时操作加亮和置顶的时候，会提供添加两次置顶图章的问题，重新方法：后台设置置顶图章关联置顶操作，然后同时置顶和加亮操作。
    Signed-off-by: Hmily_LCG <68857640@qq.com>

--- upload/source/include/topicadmin/topicadmin_moderate.php
+++ upload/source/include/topicadmin/topicadmin_moderate.php
@@ -155,7 +155,7 @@ if(!submitcheck('modsubmit')) {
 	} else {
 		$images = array();
 		foreach($operations as $operation) {
-
+            $stampstatus = 0;
 			$updatemodlog = TRUE;
 			if($operation == 'stick') {
 				$sticklevel = intval($_GET['sticklevel']);
