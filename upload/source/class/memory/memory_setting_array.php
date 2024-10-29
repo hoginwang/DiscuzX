4ce00d2da1bd8523e73b38c8c7031d4c469e2885
ruinjoy <ruinjoy@163.com>
2024-10-29 09:06:50 +0000

    修复memcached
    Signed-off-by: ruinjoy <ruinjoy@163.com>

--- upload/source/class/memory/memory_setting_array.php
+++ upload/source/class/memory/memory_setting_array.php
@@ -62,8 +62,15 @@ class memory_setting_array implements ArrayAccess {
  		$this->can_lazy = C::memory()->goteval && C::memory()->gothash;
 		if (!$this->can_lazy) { // 不支持lazy load的时候，直接加载整个数据
 			$this->array = memory('get', self::SETTING_KEY);
-			foreach ($this->array as $key => $value) {
-				if ($value) $this->array[$key] = dunserialize($value);
+			// 检查值是否有效，避免处理 null 和空字符串
+			if ($value !== null && $value !== '') {
+                $unserializedValue = dunserialize($value);
+        
+                // 检查反序列化是否成功
+                if ($unserializedValue !== false) {
+                    $this->array[$key] = $unserializedValue; // 仅在成功时赋值
+                }
+                // 如果反序列化失败，保持原值
 			}
 		}
 	}
@@ -122,7 +129,7 @@ class memory_setting_array implements ArrayAccess {
 			}
 			memory('hmset', self::SETTING_KEY, $newdata);
 		} else {
-			memory('set', $data);
+            memory('set', self::SETTING_KEY, $data);//memcached不支持key为array
 		}
 	}
 
