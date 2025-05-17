801ac2b273c99690b53c6febf717347708ef4e74
湖中沉 <341936031@qq.com>
2025-05-17 12:06:48 +0000

    手机版支持密码帖
    Signed-off-by: 湖中沉 <341936031@qq.com>

--- upload/static/js/mobile/common.js
+++ upload/static/js/mobile/common.js
@@ -955,3 +955,13 @@ function copycode(obj) {
 function setanswer(tid, pid, from, formhash){
 	popup.open('您确认要把该回复选为“最佳答案”吗？','confirm','forum.php?mod=misc&action=bestanswer&tid=' + tid + '&pid=' + pid + '&from=' + from + '&bestanswersubmit=yes&formhash='+formhash)
 }
+
+function submitpostpw(pid, tid) {
+	var obj = document.getElementById('postpw_' + pid);
+	setcookie('postpw_' + pid, hex_md5(obj.value));
+	if(!tid) {
+		location.href = location.href;
+	} else {
+		location.href = 'forum.php?mod=viewthread&tid='+tid;
+	}
+}
