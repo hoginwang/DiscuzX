0279bb339bb7545ec3c90e789aff97739b44c6e9
橘猫 <14495495+small-ginger-cat@user.noreply.gitee.com>
2025-06-11 05:29:05 +0000

    修复带有modthreadkey且具有[page]分页的帖子无法正常翻页的问题
    Signed-off-by: 橘猫 <14495495+small-ginger-cat@user.noreply.gitee.com>

--- upload/source/module/forum/forum_viewthread.php
+++ upload/source/module/forum/forum_viewthread.php
@@ -1254,7 +1254,7 @@ function viewthread_procpost($post, $lastvisit, $ordertype, $maxposition = 0) {
 						$post['message'] = preg_replace("/\s?\[page\]\s?/is", '', $post['message']);
 					}
 					if($_GET['cp'] != 'all' && strpos($post['message'], '[/index]') === FALSE && empty($_GET['threadindex']) && !$messageindex) {
-						$_G['forum_posthtml']['footer'][$post['pid']] .= '<div id="threadpage"></div><script type="text/javascript" reload="1">show_threadpage('.$post['pid'].', '.$cp.', '.count($messagearray).', '.($_GET['from'] == 'preview' ? '1' : '0').');</script>';
+						$_G['forum_posthtml']['footer'][$post['pid']] .= '<div id="threadpage"></div><script type="text/javascript" reload="1">show_threadpage(' . $post['pid'] . ', ' . $cp . ', ' . count($messagearray) . ', ' . ($_GET['from'] == 'preview' ? '1' : '0') . ', \'' . (isset($_GET['modthreadkey']) ? $_GET['modthreadkey'] : '') . '\');</script>';
 					}
 				}
 			}
