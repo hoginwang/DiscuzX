e19d40b4716b32dce5cc209317a33f8b840620f1
橘猫 <14495495+small-ginger-cat@user.noreply.gitee.com>
2025-06-25 02:51:58 +0000

    补充发帖时header中的缓存控制字段。
    Signed-off-by: 橘猫 <14495495+small-ginger-cat@user.noreply.gitee.com>

--- upload/source/app/forum/child/post/newthread.php
+++ upload/source/app/forum/child/post/newthread.php
@@ -169,6 +169,11 @@ if(!submitcheck('topicsubmit', 0, $seccodecheck, $secqaacheck)) {
 			table_forum_forum::t()->update_commoncredits(intval($mygroupid[0]));
 		}
 	}
+
+	@header("Expires: -1");
+	@header("Cache-Control: no-store, private, post-check=0, pre-check=0, max-age=0", FALSE);
+	@header("Pragma: no-cache");
+
 	$modthread = C::m('\forum\model_thread');
 	$bfmethods = $afmethods = [];
 
