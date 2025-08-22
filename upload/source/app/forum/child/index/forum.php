7418a848436e049a02ce7bd62d5ca40835f82f40
azurekiln <11491362+azurekiln@user.noreply.gitee.com>
2025-08-22 19:04:11 +0000

    update upload/source/app/forum/child/index/forum.php. 修复else分支缺失导致主页分区和板块无法正常显示的问题。

--- upload/source/app/forum/child/index/forum.php
+++ upload/source/app/forum/child/index/forum.php
@@ -80,6 +80,8 @@ foreach($forums as $forum) {
 				$forumlist[$forum['fup']]['subforums'][] = $subforum;
 			}
 		}
+        
+	} else {
 
 		if($forum['moderators']) {
 			$forum['moderators'] = moddisplay($forum['moderators'], 'flat');
